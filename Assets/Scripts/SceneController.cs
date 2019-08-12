43fd799f7c418e5841c8692d3a93f14ac13959cc
Roman Saykin <sc2aru@gmail.com>
2019-08-13 00:02:07 +0300

    Created a simple UI menu with logic
    Created a simple UI menu with 3 buttons and simple logic inside each.

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class SceneController : MonoBehaviour
+{
+    public Canvas creditsPopup;
+
+    private void Start()
+    {
+        creditsPopup.enabled = false;
+    }
+
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnCreditsButtonClick()
+    {
+        creditsPopup.enabled = true;
+    }
+
+    public void OnExitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    public void OnClosePopupButton()
+    {
+        creditsPopup.enabled = false;
+    }
+
+}
